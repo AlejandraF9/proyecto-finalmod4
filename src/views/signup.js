0f1e19151cf3e1795b10c56f23e4ea7aec29a7a7
AlejandraF9 <alejandra.f.c.95@gmail.com>
2025-07-01 17:00:56 +0100

    Update signup.js

--- src/views/signup.js
+++ src/views/signup.js
@@ -12,7 +12,8 @@ newUserdiv.innerHTML =`
     <input type="text" id="registerFormName" placeholder="Name" required>
     <input type="email" id="registerFormEmail" placeholder="Email" required>
     <input type="password" id="registerFormPassword" placeholder="Password" required>
-    <input type="text" id="registerFormCountry" placeholder="Country">
+    <input type="password" id="registerFormRepPassword" placeholder = "Repeat password" required>
+    <input type="text" id="registerFormIsland" placeholder="Island">
     <button type="submit">Sign up</button>
 </form>`
 
@@ -20,30 +21,22 @@ const formNewuser = document.getElementById("divNewUserForm")
 
 formNewuser.addEventListener ("submit", async (event) => {
     event.preventDefault();
-    const name = document.getElementById("registerFormName").value.trim();
-    const email = document.getElementById("registerFormEmail").value.trim();
-    const password = document.getElementById("registerFormPassword").value.trim();
-    const country = document.getElementById("registerFormCountry").value.trim();
+    const signupName = document.getElementById("registerFormName").value.trim();
+    const signupEmail = document.getElementById("registerFormEmail").value.trim();
+    const signupPassword = document.getElementById("registerFormPassword").value.trim();
+    const signupRepPassword = document.getElementById("registerFormRepPassword").value.trim();
+    const signupIsland = document.getElementById("registerFormIsland").value.trim();
 
-    const validations = dataValidations({name: name, email: email, password: password, /*country: country*/});
+    const validations = dataValidations({name: signupName, email: signupEmail, password: signupPassword});
     
     if(validations) {
       const userData = {
       name,
       email,
       password,
-      /*country,*/
     };
 
    await createNewUser(userData);
 }
-
-    // createNewUser({
-    //     name,
-    //     email,
-    //     password,
-    //     country
-    // })
-
 })
 
